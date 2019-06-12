b91f04fa8c037f8113084e5933d2996861e82fb9
karpimpski <mike.karpinski99@gmail.com>
2019-06-11 23:46:29 -0400

    Fixed movement

--- Scripts/Entity.gd
+++ Scripts/Entity.gd
@@ -6,35 +6,23 @@ func _ready():
 	cell_size = Global.cell_size
 
 func move(direction):
-	var motion = Vector2()
-	
-	if direction == Vector2(0, -1):		# up
-		rotation_degrees = 180
-		motion.y = -cell_size
-	elif direction == Vector2(1, -1):	# up-right
-		rotation_degrees = 225
-		motion.x = cell_size
-		motion.y = -cell_size
-	elif direction == Vector2(1, 0):	# right
-		rotation_degrees = 270
-		motion.x = cell_size
-	elif direction == Vector2(1, 1):	# down-right
-		rotation_degrees = 315
-		motion.x = cell_size
-		motion.y = cell_size
-	elif direction == Vector2(0, 1):	# down
-		rotation_degrees = 0
-		motion.y = cell_size
-	elif direction == Vector2(-1, 1):	# down-left
-		rotation_degrees = 45
-		motion.x = -cell_size
-		motion.y = cell_size
-	elif direction == Vector2(-1, 0):	# left
-		rotation_degrees = 90
-		motion.x = -cell_size
-	elif direction == Vector2(-1, -1):	# up-left
-		rotation_degrees = 135
-		motion.x = -cell_size
-		motion.y = -cell_size
-	
-	move_and_collide(motion)
+	if !test_move(transform.translated(direction * cell_size), Vector2()):
+		var motion = Vector2()
+		
+		if direction == Vector2(0, -1):		# up
+			$AnimatedSprite.rotation_degrees = 180
+		elif direction == Vector2(1, -1):	# up-right
+			$AnimatedSprite.rotation_degrees = 225
+		elif direction == Vector2(1, 0):	# right
+			$AnimatedSprite.rotation_degrees = 270
+		elif direction == Vector2(1, 1):	# down-right
+			$AnimatedSprite.rotation_degrees = 315
+		elif direction == Vector2(0, 1):	# down
+			$AnimatedSprite.rotation_degrees = 0
+		elif direction == Vector2(-1, 1):	# down-left
+			$AnimatedSprite.rotation_degrees = 45
+		elif direction == Vector2(-1, 0):	# left
+			$AnimatedSprite.rotation_degrees = 90
+		elif direction == Vector2(-1, -1):	# up-left
+			$AnimatedSprite.rotation_degrees = 135
+		position += direction * cell_size
